d09ef5d5709b5c8e67d6026d6107dfcf4c102b8b
abhinavash <76036114+abhinavash@users.noreply.github.com>
2022-12-31 12:21:46 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
   // fetch data from server
 
-  const response = await fetch('http://localhost:5000' ,{
+  const response = await fetch('https://chat-ai-backend-production.up.railway.app/' ,{
     method: 'POST',
     headers: {
       'Content-Type' : 'application/json'
